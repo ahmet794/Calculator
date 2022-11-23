2c3f60bd1a8f0cb41e283d2c73df44a913c2e893
Ahmet Cihan <ahmet@Ahmets-MacBook-Pro.local>
2022-11-23 18:04:16 +0000

    Created the new feature branch a new test case and the new class NumStack.

---
+++ TTDdemo/src/main/java/uk/ac/rhul/cs2800/NumStack.java
@@ -0,0 +1,24 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * We facade the details by creating the NumStack class which creates Stacks that only takes floats.
+ * 
+ * @author ahmet
+ *
+ */
+public class NumStack {
+
+  private Stack numStack;
+
+  /**
+   * This constructor creates a Stack object.
+   * 
+   */
+  public NumStack() {
+
+    numStack = new Stack();
+  }
+  
+  
+
+}
---
+++ TTDdemo/src/test/java/uk/ac/rhul/cs2800/NumStackTest.java
@@ -0,0 +1,19 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumStackTest {
+
+  private NumStack numStack;
+  private EntryInt value;
+  
+  @BeforeEach
+  void setup() {
+    numStack = new NumStack();
+    value = new FloatEntry(17.0f);
+  }
+  
+
+}
